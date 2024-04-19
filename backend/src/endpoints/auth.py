965d80ffe3251272502f1b21860030f52e2228ae
NBernkastel <nexeland@gmail.com>
2024-04-19 10:42:29 +0600

    445

--- backend/src/endpoints/auth.py
+++ backend/src/endpoints/auth.py
@@ -1,17 +1,25 @@
 from typing import Annotated
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
+from fastapi.security import OAuth2PasswordRequestForm
 
 from schemes.schemes import UserReg, UserLog
 from services.user import UserService
+from utils.auth import verify_password
 from utils.dependens import user_service_fabric
 
 auth_router = APIRouter(prefix='/auth', tags=['Auth'])
 
 
-@auth_router.post('/login')
-def login(user: UserLog):
-    print(user)
+@app.post('/token')
+def login(user: Annotated[OAuth2PasswordRequestForm, Depends()]):
+    db_user = user_collection.find_one({"username": user.username})
+    if db_user is None:
+        raise HTTPException(status_code=401, detail="Incorrect username or password")
+    if verify_password(db_user["password"].encode(), user.password):
+        return Token(access_token=generate_token(db_user["username"]), token_type="bearer")
+    else:
+        raise HTTPException(status_code=401, detail="Incorrect username or password")
 
 
 @auth_router.post('/register')
--- backend/src/utils/auth.py
+++ backend/src/utils/auth.py
@@ -1,5 +1,17 @@
+from datetime import datetime, timedelta
+
 import bcrypt
 import jwt
+from fastapi import HTTPException
+from jwt import encode, decode
+
+
+def auth_user(token: str):
+    try:
+        username = verify_token(token)
+    except jwt.PyJWTError:
+        raise HTTPException(status_code=401, detail="wrong token")
+    return username
 
 
 def hash_password(password):
@@ -11,3 +23,27 @@ def hash_password(password):
 
 def verify_password(hashed_password, input_password):
     return bcrypt.checkpw(input_password.encode('utf-8'), hashed_password)
+
+
+def generate_token(username: str, del_time: int = 30) -> str:
+    if not isinstance(username, str):
+        raise ValueError("username should be string")
+    expiration = datetime.utcnow() + timedelta(hours=del_time)
+    payload = {
+        'username': username,
+        'exp': expiration
+    }
+    token: str = encode(payload, SECRET_KEY, algorithm='HS256')
+    return token
+
+
+def verify_token(token: str) -> jwt.PyJWTError or str:
+    try:
+        payload = decode(token, SECRET_KEY, algorithms=['HS256'])
+        if payload['exp'] < datetime.utcnow().timestamp():
+            raise jwt.ExpiredSignatureError("token expired")
+
+    except (jwt.DecodeError, jwt.ExpiredSignatureError):
+        raise jwt.PyJWTError
+
+    return payload['username']
